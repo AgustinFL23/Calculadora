840d5545499e1d5d22faef8facc9792c2abefc14
AgustinFL23 <127245897+AgustinFL23@users.noreply.github.com>
2024-02-22 12:54:28 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+float suma(float[] sumandos){} //eligan la que quieran hacer
+float resta(float[] sumandos){}
+float multiplicacion(float[] sumandos){}
+float division(float[] sumandos){}
 int main(){
   printf("Seleccione la operacion que desea realizar/n");
   
