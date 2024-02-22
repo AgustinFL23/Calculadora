357574b9cc1979f8bf8eddfb034e7a603d5f0030
oscart0613 <160933319+oscart0613@users.noreply.github.com>
2024-02-22 13:02:17 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -6,6 +6,9 @@ float multiplicacion(float* multiplicando){}
 float division(float dividendo, divisor){}
 int main(){
   printf("Seleccione la operacion que desea realizar/n");
-  
+printf("Ingrese el primer numero");
+scanf("%f, &num1);
+printf("Ingrese el segundo numero");
+  scanf("%f, &num2)
   return 0;
 }
