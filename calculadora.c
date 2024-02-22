d12956ba90de5db57056a0eff8007eec734d9543
AgustinFL23 <127245897+AgustinFL23@users.noreply.github.com>
2024-02-22 13:06:21 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -5,7 +5,8 @@ float resta(float* resta){}
 float multiplicacion(float* multiplicando){}
 float division(float dividendo, divisor){}
 int main(){
-  int op;
+  int op=6;
+  while(op!=5{
   printf("Seleccione la operacion que desea realizar/n");
   printf("1.- Suma");
   printf("2.-Resta");
@@ -13,10 +14,14 @@ int main(){
   printf("4.-División);
   printf("5.-Salir");
   scanf("%d",&op);
+  switch(op){
+    
+  }
   //para añadir en la opcion de multiplicación
 printf("Ingrese el primer numero");
 scanf("%f, &num1);
 printf("Ingrese el segundo numero");
 scanf("%f, &num2)
+  }
   return 0;
 }
